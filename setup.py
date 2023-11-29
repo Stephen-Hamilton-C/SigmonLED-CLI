0f5e6ef10b70a2d10768a9ccd149ed4c913f0553
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2023-11-29 17:47:18 -0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sigmonled',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/Stephen-Hamilton-C/SigmonLED-CLI',
+    license='GPL-3.0',
+    author='Stephen Hamilton',
+    author_email='stephen.hamilton.c@gmail.com',
+    description=''
+)
